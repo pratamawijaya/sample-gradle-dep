63814ff4cf6a8f2004c89df0716ec28178d1d5c3
pratamawijaya <set.mnemonix@gmail.com>
2019-01-10 14:37:48 +0700

    add app compat library

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,14 @@
+object Versions {
+    val kotlin = "1.2.71"
+    val appcompat = "28.0.0"
+    val constraintlayout = "1.1.3"
+}
+
+object Dependencies {
+    val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
+
+object SupportLibraries {
+    val appcompat = "com.android.support:appcompat-v7:${Versions.appcompat}"
+    val constraintlayout = "com.android.support.constraint:constraint-layout:${Versions.constraintlayout}"
+}
